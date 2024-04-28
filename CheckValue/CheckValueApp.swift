ba19810dc5b1dbab6e3628d1a943bf0390b88e73
vsvami <106614918+vsvami@users.noreply.github.com>
2024-04-28 23:10:25 +0400

    Initial Commit

---
+++ CheckValue/CheckValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckValueApp.swift
+//  CheckValue
+//
+//  Created by Vladimir Dmitriev on 28.04.24.
+//
+
+import SwiftUI
+
+@main
+struct CheckValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckValue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckValue
+//
+//  Created by Vladimir Dmitriev on 28.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
